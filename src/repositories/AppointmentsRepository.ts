f8494c018ead2f269265a163c1e55f495a68709f
Davi Souza <davissbf@gmail.com>
2021-01-10 22:17:00 -0300

    conexaoComBancoDeDados

--- src/repositories/AppointmentsRepository.ts
+++ src/repositories/AppointmentsRepository.ts
@@ -1,36 +1,18 @@
-import { isEqual } from 'date-fns';
+import { EntityRepository, Repository } from 'typeorm';
+
 import Appointment from '../models/Appointment';
 
-interface CreateAppointmentDTO {
-  provider: string;
-  date: Date;
-}
+@EntityRepository(Appointment)
+class AppointmentsRepository extends Repository<Appointment> {
+  public async findByDate(date: Date): Promise<Appointment | null> {
+    // const findAppointement = this.appointments.find(appointment =>
+    //   isEqual(date, appointment.date),
 
-class AppointmentsRepository {
-  private appointments: Appointment[];
-
-  constructor() {
-    this.appointments = [];
-  }
-
-  public all(): Appointment[] {
-    return this.appointments;
-  }
-
-  public findByDate(date: Date): Appointment | null {
-    const findAppointement = this.appointments.find(appointment =>
-      isEqual(date, appointment.date),
-    );
+    const findAppointement = await this.findOne({
+      where: { date },
+    });
 
     return findAppointement || null;
-  }
-
-  public create({ provider, date }: CreateAppointmentDTO): Appointment {
-    const appointment = new Appointment({ provider, date });
-
-    this.appointments.push(appointment);
-
-    return appointment;
   }
 }
 
